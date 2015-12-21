013349359244e000e9a0c528810debc9d5163f5f
Hannes Kirsman <hkirsman@gmail.com>
2015-12-21 23:57:08 +0200

    Create unix_batch_image_renamer.sh

---
+++ unix_batch_image_renamer.sh
@@ -0,0 +1,11 @@
+for i in *.jpg; do
+  md5=`md5deep "$i" | cut -c 1-7`.jpg
+  date_raw=`exif --tag="Date and Time (digitized)" "$i" | sed -n '6p'`
+  year=`echo "$date_raw" | cut -c 10-13`
+  month=`echo "$date_raw" | cut -c 15-16`
+  day=`echo "$date_raw" | cut -c 18-19`
+  hour=`echo "$date_raw" | cut -c 21-22`
+  minute=`echo "$date_raw" | cut -c 24-25`
+  second=`echo "$date_raw" | cut -c 27-28`
+  mv -i "$i" "$year-$month-$day"_"$hour-$minute-$second"_"$md5"
+done
