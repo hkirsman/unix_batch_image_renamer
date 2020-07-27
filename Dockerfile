d8a1dd7eddf4b9a3ddb3d354c635b0f275287f8d
Hannes Kirsman <hannes.kirsman@wunder.io>
2020-07-27 14:29:51 +0300

    #2: Add heic support + some other script improvements.

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ ADD unix_batch_image_renamer.sh /usr/local/bin/unix_batch_image_renamer.sh
 
 RUN apt-get update
 
-RUN apt-get install -y exif md5deep
+## mmv needed to rename file if only case is different.
+RUN apt-get install -y exiftool md5deep mmv
 
 ENTRYPOINT ["unix_batch_image_renamer.sh"]
--- unix_batch_image_renamer.sh
+++ unix_batch_image_renamer.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
-for i in *.[Jj][Pp][Gg]; do
-  md5=`md5deep "$i" | cut -c 1-7`.jpg
-  date_raw=`exif --tag="DateTimeOriginal" "$i" | sed -n '6p'`
-  year=`echo "$date_raw" | cut -c 10-13`
-  month=`echo "$date_raw" | cut -c 15-16`
-  day=`echo "$date_raw" | cut -c 18-19`
-  hour=`echo "$date_raw" | cut -c 21-22`
-  minute=`echo "$date_raw" | cut -c 24-25`
-  second=`echo "$date_raw" | cut -c 27-28`
-  mv -i "$i" "$year-$month-$day"_"$hour-$minute-$second"_"$md5"
+#
+# Script to rename any jpg, jpeg, heic file to to contain original date and
+# also append unique string with md5 hash of the file.
+#
+
+# Lowercase all files.
+mmv '*' '#l1'
+
+# Normalize jpeg file extensions.
+mmv '*.jpeg' '#1.jpg' > /dev/null 2>&1
+
+# Better foreach for looping through files:
+# https://askubuntu.com/questions/343727/filenames-with-spaces-breaking-for-loop-find-command/343753#343753
+#
+# Looping through files by checking multiple extensions.
+# https://unix.stackexchange.com/questions/15308/how-to-use-find-command-to-search-for-multiple-extensions
+find . -maxdepth 1 -type f \( -iname \*.jpg -o -iname \*.heic \) -print0 |
+while IFS= read -r -d '' file; do
+  md5=`md5deep "$file" | cut -c 1-7`
+  file_base=$(basename -- "$file")
+  extension="${file_base##*.}"
+  date_raw=`exiftool -DateTimeOriginal "$file"`
+  year=`echo "$date_raw" | cut -c 35-38`
+  month=`echo "$date_raw" | cut -c 40-41`
+  day=`echo "$date_raw" | cut -c 43-44`
+  hour=`echo "$date_raw" | cut -c 46-47`
+  minute=`echo "$date_raw" | cut -c 49-50`
+  second=`echo "$date_raw" | cut -c 52-53`
+  new_file_name="$year-$month-$day"_"$hour-$minute-$second"_"$md5"."$extension"
+
+  # Start renaming but only if file name is correct.
+  # We can make the assumption from file name length because the file has
+  # either correct name:
+  # 2020-07-12_00-17-04_3d52b8b.jpg
+  # or not correct name:
+  # --_--_0eb7443.jpg
+  # So let's assume if file name is longer than 17, we can start renaming it.
+  new_file_name_length=${#new_file_name}
+  if [ $new_file_name_length -gt 17 ]
+  then
+    mmv -d "$file" "$new_file_name"
+  fi
 done
